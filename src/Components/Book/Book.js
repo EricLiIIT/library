d90d561b3d79806c51eed27ac4b0098ea558a021
Eric <eli7@hawk.iit.edu>
2023-08-31 00:50:56 -0500

    feat: show checkbox as checked if already read, empty if not. Not interactive yet

--- src/Components/Book/Book.js
+++ src/Components/Book/Book.js
@@ -1,16 +1,23 @@
 import React from "react";
-import { BiTrashAlt } from "react-icons/bi";
+import { BiTrashAlt, BiSolidCheckboxChecked, BiCheckbox, BiCheckboxChecked } from "react-icons/bi";
 
 export default function Book(props) {
   function handleDelete() {
     props.handleDelete();
   }
 
+  function handleCheck() {
+    return 
+  }
   return (
     <div className={props.view === "row" ? "row" : "card"}>
       <div className="title">{props.book.title}</div>
       <div className="author">{props.book.author}</div>
       <div className="pages">{props.book.pages}</div>
+      <div className="readStatus">Read</div>
+      <div className="checkbox">
+        {(props.book.read) ? <BiCheckbox/> : <BiCheckboxChecked/>}
+      </div>
       <div className="trash">
         <BiTrashAlt onClick={handleDelete} />
       </div>
