c92505a2118782ecd5898a4fccaed11aa11c5ce2
Eric <eli7@hawk.iit.edu>
2023-09-11 23:08:25 -0500

    tweak: limit query to library api to 5 books

--- src/Services/GetBookData.js
+++ src/Services/GetBookData.js
@@ -1,6 +1,6 @@
 export async function getBookData(title) {
   let formattedTitle = title.split(' ').join('+');
-  const url = `https://openlibrary.org/search.json?q=${formattedTitle}`;
+  const url = `https://openlibrary.org/search.json?q=${formattedTitle}&limit=5`;
   const response = await fetch(url);
   const data = await response.json();
   console.log(formattedTitle, url)
